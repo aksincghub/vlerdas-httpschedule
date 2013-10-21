b796770a382c39319ddfb8d8c4a967e12f37cedc
Julian Jewel <julian@julianjewel.com>
2013-10-20 17:30:39 -0700

    Upgraded Logging and Retry

--- schedule.js
+++ schedule.js
@@ -1,5 +1,6 @@
 /**
  * Entry point for the Scheduler to call a REST URL
+ * Check the config file for more information.
  *
  * Created by: Julian Jewel
  *
@@ -7,44 +8,68 @@
 var config = require('config')
 // Export config, so that it can be used anywhere
 module.exports.config = config;
+// For making HTTP Requests
 var request = require('request');
+// CRON Scheduler
 var cronJob = require('cron').CronJob;
+// Logging Framework
+var Log = require('vcommons').log;
+// Scheduler as the Category type
+var logger = Log.getLogger('SCHEDULER', config.log);
+// For retry capability with the HTTP Request
+var retry = require('retry');
 
 try {
+	logger.trace("Initializing Job: " + config.schedule + " Start: " + config.start + " TimeZone: " + config.timeZone );
 	var job = new cronJob(config.schedule, function(){
-		if(config.debug)
-			console.log("Starting Job - Executing: " + config.uri);
-		request(config.uri, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				return;
-			} else {
-				throw error;
-			}
+		logger.trace("Starting Job - Executing: " + config.request);
+		// Retry on failure
+		var operation = retry.operation(config.retry);
+		operation.attempt(function(currentAttempt) {
+			// HTTP Request
+			request(config.request, function (error, response, body) {
+				var bError = true;
+				if(response) {
+					bError = (response.statusCode != 200);
+				}
+				if (operation.retry(error || bError)) {
+					logger.error('Retry failed with error:', error, 'Attempt:', currentAttempt);
+					return;
+				}
+				if (!error && response.statusCode == 200) {
+					logger.info('Successfully Completed Job', response, 'Attempt:', currentAttempt);
+					return;
+				} else {
+					logger.error('Retry failed with error:', error, 'Attempt:', currentAttempt);
+					return;
+				}
+			});
 		});
 	  }, function () {
-		if(config.debug)
-			console.log("Finished Execution!");
+			// Completed
+			logger.trace('Finished Execution of ' + config.request);
 	  }, 
 	  config.start /* Start the job right now */,
 	  config.timeZone /* Time zone of this job. */
 	);
-	
+	logger.info("Starting Scheduler Job with Schedule: " + config.schedule + " Start: " + config.start + " TimeZone: " + config.timeZone );
 	job.start();
 } catch(ex) {
-	console.log("Cron pattern + " + config.scheduel + "not valid");
+	logger.error("Cron pattern + " + config.schedule + " not valid. Error: " + ex);
 }
 	
 
 // Default exception handler
 process.on('uncaughtException', function (err) {
-	console.log('Caught exception: ' + err);
+	logger.error("Caught Exception " + err);
 });
 
 process.on('SIGINT', function () {
-	console.log("\nShutting down from  SIGINT (Crtl-C)")
+	logger.info("\nShutting down from  SIGINT (Crtl-C) ");
 	process.exit()
 })
 // Default exception handler
 process.on('exit', function (err) {
 	// Clean up
+	logger.info("Exiting with Error:" + err);
 });
